05f0160b5a3491e02bd2f97671523e6235d5c0be
HuSharp <19973658602@163.com>
2020-10-21 02:10:19 +0800

    finished chapter12_Syacall_And_heapgit status

---
+++ c12/free/userprog/syscall-init.c
@@ -0,0 +1,38 @@
+#include "syscall-init.h"
+#include "syscall.h"
+#include "stdint.h"
+#include "print.h"
+#include "thread.h"
+#include "console.h"
+#include "string.h"
+
+
+// 定义 调用子功能数组
+#define syscall_nr 32
+typedef void* syscall;
+syscall syscall_table[syscall_nr];
+
+/*********   内核空间中的函数 + sys   ***************/
+// 返回当前任务的 pid 
+uint32_t sys_getpid(void) {
+   return running_thread()->pid;
+}
+
+// 打印字符串（未实现文件系统前版本）
+// 返回字符串长度
+uint32_t sys_write(char* str) {
+   console_put_str(str);
+   return strlen(str);
+}
+
+/********************************************/
+
+// 初始化系统调用
+void syscall_init(void) {
+   put_str("syscall_init start!\n");
+   syscall_table[SYS_GETPID] = sys_getpid;
+   syscall_table[SYS_WRITE] = sys_write;
+   syscall_table[SYS_MALLOC] = sys_malloc;
+   syscall_table[SYS_FREE] = sys_free;
+   put_str("syscall_init done!\n");
+}
---
+++ c12/malloc/lib/user/syscall.h
@@ -0,0 +1,12 @@
+#ifndef __LIB_USER_SYSCALL_H
+#define __LIB_USER_SYSCALL_H
+#include "stdint.h"
+
+// 表示系统调用功能号
+enum SYSCALL_NR {
+    SYS_GETPID,
+    SYS_WRITE
+};
+uint32_t getpid(void);
+uint32_t write(char* str);
+#endif
