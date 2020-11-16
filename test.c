6b289b57c83d9471cd9d2009ab3a2a1a9568b178
HuSharp <19973658602@163.com>
2020-11-16 20:03:12 +0800

    finished chap_14_traverse of directory!

--- test.c
+++ test.c
@@ -1,9 +1,18 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "dirent.h"
 
 int main()
 {
-    char *a = "1";
-    while(*(a++) != 0) {
-        printf("Null!\n");
+    DIR* p_dir = NULL;
+    struct dirent* dir_e = NULL;
+
+    p_dir = opendir("/");
+    if(p_dir) {
+        while(dir_e = readdir(p_dir)) {
+            printf("%s ", dir_e->d_name);
+        }
+        printf("\n");
     }
+    return 0;
 }
